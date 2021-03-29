a51ea3cb87f0d96016b444a1e1ea23986f7fa989
bruteforce91 <corsarosalvatore91@yahoo.it>
2021-03-29 18:08:00 +0200

    fix navBar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,23 @@
-import { Link } from "@chakra-ui/react"
+import { Link } from "@chakra-ui/react";
 
 import React from "react";
 
 function NavBar() {
   return (
     <div>
-      <Link color="blue">Home</Link>
-      <Link>About</Link>
-      <Link>Contact</Link>
+      <Breadcrumb separator="-">
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">About</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink href="#">Contact</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
     </div>
   );
 }
